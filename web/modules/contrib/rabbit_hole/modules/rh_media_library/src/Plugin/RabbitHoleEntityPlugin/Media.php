8fc73224735ff25e71c69ff8581efb28f36a990f
Nhung Le <nhung.le@ffwagency.com>
2019-06-13 15:04:41 +0700

    Patched rh_media_library_support.patch file for rabbit_hole module

---
+++ web/modules/contrib/rabbit_hole/modules/rh_media_library/src/Plugin/RabbitHoleEntityPlugin/Media.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Drupal\rh_media_library\Plugin\RabbitHoleEntityPlugin;
+
+use Drupal\rabbit_hole\Plugin\RabbitHoleEntityPluginBase;
+
+/**
+ * Implements rabbit hole behavior for media library.
+ *
+ * @RabbitHoleEntityPlugin(
+ *   id = "rh_media_library",
+ *   label = @Translation("Media Library"),
+ *   entityType = "media"
+ * )
+ */
+class Media extends RabbitHoleEntityPluginBase {
+
+}
